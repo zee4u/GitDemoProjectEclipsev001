3eec51108fcf8fa237dc3c7ef877e2290dd94333
Arslan Mughal <tim@timrich.org>
2024-06-01 15:35:52 +0500

    this is my first commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hungry="yes";
+
+if(hungry=="yes")
+
+	System.out.println("eat something...........");
+	
+else {
+	System.out.println("Do something...........");
+
+	}
+
+}
+}
